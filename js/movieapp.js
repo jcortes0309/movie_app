2dba540f352867f15af2d8d45260fd44f743f56c
Juan A Cortes <juan.cortes@gmail.com>
2016-11-16 13:21:07 -0500

    Step 2: Search Page - Implement the search page with a big search input field in the middle like the Google search page.

--- js/movieapp.js
+++ js/movieapp.js
@@ -1,15 +1,29 @@
 var app = angular.module("movie-search", ["ui.router"]);
 
+app.controller("MainController", function($scope, $stateParams, $state) {
+  $scope.search = function() {
+    $scope.query = $scope.searchMovies;
+    // $scope.pageName = $stateParams.page_name;
 
+    $state.go("search", { query: $scope.pageName } );
+
+    console.log("Hit enter in the search input with value of: ", $scope.query);
+  };
+});
 
 app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state({
       name: "home",
-      url: "/"
-      // templateUrl: "home.html",
-      // controller: "HomeController"
+      url: "/search",
+      templateUrl: "search.html",
+      controller: "MainController"
+    })
+    .state({
+      name: "search",
+      url: "/search/{query}",
+      templateUrl: "search_results.html"
     });
 
-    $urlRouterProvider.otherwise("/");
+    $urlRouterProvider.otherwise("/search");
 });
